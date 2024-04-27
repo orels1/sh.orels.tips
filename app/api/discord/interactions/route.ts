46ab14beccaa8fda67fd4c31fea09e3042ba2310
orels1 <ping@orels.sh>
2024-04-27 15:41:28 +0400

    added better error handling

--- app/api/discord/interactions/route.ts
+++ app/api/discord/interactions/route.ts
@@ -3,11 +3,16 @@ import nacl from 'tweetnacl';
 
 export async function POST(request: NextRequest)
 {
+  const bodyText = await request.text();
   const signature = request.headers.get('X-Signature-Ed25519') as string;
   const timestamp = request.headers.get('X-Signature-Timestamp') as string;
 
+  if (!bodyText || !signature || !timestamp){
+    return NextResponse.json({ message: 'Invalid request' }, { status: 400 });
+  }
+
   const isValid = nacl.sign.detached.verify(
-    new TextEncoder().encode(timestamp + await request.text()),
+    new TextEncoder().encode(timestamp + bodyText),
     Buffer.from(signature, 'hex'),
     Buffer.from(process.env.DISCORD_APP_PUBLIC_KEY!, 'hex')
   );
@@ -17,7 +22,7 @@ export async function POST(request: NextRequest)
     return NextResponse.json({ message: 'Invalid request' }, { status: 401 });
   }
 
-  const body = await request.json() as { type: number; };
+  const body = JSON.parse(bodyText) as { type: number; };
 
   if (!body || body.type !== 1)
   {
