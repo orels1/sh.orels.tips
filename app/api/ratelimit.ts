e92aa07a1a641f628054fa8804bd7b0803ba3bbd
orels1 <ping@orels.sh>
2024-04-27 17:34:54 +0400

    ratelimit to 1 per second

--- app/api/ratelimit.ts
+++ app/api/ratelimit.ts
@@ -3,7 +3,7 @@ import { Redis } from "@upstash/redis";
 
 export const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(1, "10 s"),
+  limiter: Ratelimit.slidingWindow(1, "1 s"),
   analytics: true,
   prefix: "@upstash/ratelimit",
 });
